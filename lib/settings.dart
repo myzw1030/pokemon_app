53ed5715c6ed1b9cc22e3493194014f35bae5f4d
miyazawa <13tmtmtommy@gmail.com>
2023-10-12 10:09:32 +0900

    settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,17 +1,92 @@
 import 'package:flutter/material.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({super.key});
 
   @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  ThemeMode _themeMode = ThemeMode.system;
+  @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
+      children: [
         ListTile(
-          leading: Icon(Icons.lightbulb),
-          title: Text('Dark/Light Mode'),
+          leading: const Icon(Icons.lightbulb),
+          title: const Text('Dark/Light Mode'),
+          trailing: Text((_themeMode == ThemeMode.system)
+              ? 'System'
+              : (_themeMode == ThemeMode.dark ? 'Dark' : 'Light')),
+          onTap: () async {
+            var ret = await Navigator.of(context).push<ThemeMode>(
+              MaterialPageRoute(
+                builder: (context) => ThemeModeSelectionPage(init: _themeMode),
+              ),
+            );
+            setState(() => _themeMode = ret!);
+          },
         ),
       ],
+    );
+  }
+}
+
+class ThemeModeSelectionPage extends StatefulWidget {
+  const ThemeModeSelectionPage({
+    Key? key,
+    required this.init,
+  }) : super(key: key);
+  final ThemeMode init;
+
+  @override
+  State<ThemeModeSelectionPage> createState() => _ThemeModeSelectionPageState();
+}
+
+class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
+  late ThemeMode _current;
+  @override
+  void initState() {
+    super.initState();
+    _current = widget.init;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop<ThemeMode>(context, _current);
+                },
+              ),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: _current,
+              title: const Text('System'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: _current,
+              title: const Text('Dark'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: _current,
+              title: const Text('Light'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
